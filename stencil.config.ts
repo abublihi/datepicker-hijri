91fecf46b2e5758cba2d2bf52ae97a5cd212ee61
abublihi <abublihi@gmail.com>
2019-10-23 14:50:21 +0300

    update the name

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,7 +1,7 @@
 import { Config } from '@stencil/core';
 
 export const config: Config = {
-  namespace: 'datepicker-stencil',
+  namespace: 'datepicker-hijri',
   outputTargets: [
     {
       type: 'dist',
